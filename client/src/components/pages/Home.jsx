784bd442fd2a4ef4dbb5810f858962c7d6cbf79a
gracewang <gwang2@mit.edu>
2025-01-19 22:26:27 -0500

    added doodles (could replace later or add instructions text boxes)

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -4,34 +4,50 @@ import { GoogleLogin, googleLogout } from "@react-oauth/google";
 import "../../utilities.css";
 import "./Home.css";
 import { UserContext } from "../App";
+import FirstDoodle from "../../../images/beaver-with-magnifying-glass.png";
+import SecondDoodle from "../../../images/beaver-with-chart.png";
 
 const Home = () => {
   const { userId, handleLogin, handleLogout } = useContext(UserContext);
   return (
-    <div className="home-container">
-      <div className="title-box">
-        <h1 className="title">Alignment Charts</h1>
-        <p className="description">
-          Create, share, and explore alignment charts like never before.
-        </p>
+    <>
+      {/* Title Box */}
+      <div className="home-container">
+        <div className="title-box">
+          <h1 className="title">Alignment Charts</h1>
+          <p className="description">
+            Create, share, and explore alignment charts like never before.
+          </p>
+        </div>
+
+        {/* Authentication */}
+        <div className="authentication">
+          {userId ? (
+            <button
+              className="logout-button"
+              onClick={() => {
+                googleLogout();
+                handleLogout();
+              }}
+            >
+              Logout
+            </button>
+          ) : (
+            <GoogleLogin onSuccess={handleLogin} onError={(err) => console.log(err)} />
+          )}
+        </div>
       </div>
 
-      <div className="authentication">
-        {userId ? (
-          <button
-            className="logout-button"
-            onClick={() => {
-              googleLogout();
-              handleLogout();
-            }}
-          >
-            Logout
-          </button>
-        ) : (
-          <GoogleLogin onSuccess={handleLogin} onError={(err) => console.log(err)} />
-        )}
+      {/* Adding doodles to the homepage. */}
+      <div className="doodles">
+        <img
+          src={FirstDoodle}
+          alt="Beaver Holding Magnifying Glass and Peering at Chart"
+          className="left-doodle"
+        />
+        <img src={SecondDoodle} alt="Cute Beaver Holding Chart" className="right-doodle" />
       </div>
-    </div>
+    </>
   );
 };
 
